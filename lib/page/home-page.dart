e01f0225331d85642a652aa68a92ac09869748df
Fernanda Otalora <otaloraster@gmail.com>
2021-04-30 18:38:13 -0500

    Se adecua el formato

--- lib/page/home-page.dart
+++ lib/page/home-page.dart
@@ -23,6 +23,204 @@ class _HomePageState extends State<HomePage> {
     return Column(
       children: [
         Expanded(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    height: 150,
+                    width: 150,
+                    child: Card(
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Image(
+                                image: AssetImage('assets/male.png'),
+                                width: 50,
+                                height: 50),
+                            Text('Hombre',
+                                style: TextStyle(color: Colors.white70))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 150,
+                    width: 150,
+                    child: Card(
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Image(
+                                image: AssetImage('assets/male.png'),
+                                width: 50,
+                                height: 50),
+                            Text('Hombre',
+                                style: TextStyle(color: Colors.white70))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+            child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 150,
+                width: 330,
+                child: Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image(
+                            image: AssetImage('assets/male.png'),
+                            width: 50,
+                            height: 50),
+                        Text('Hombre', style: TextStyle(color: Colors.white70))
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )),
+        Expanded(
+            child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 170,
+                width: 150,
+                child: Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Peso',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                          "62",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            FloatingActionButton(
+                              backgroundColor: Colors.grey,
+                              onPressed: () {},
+                              child: Center(
+                                  child: Icon(
+                                Icons.add_rounded,
+                                color: Colors.white,
+                                size: 30,
+                              )),
+                            ),
+                            FloatingActionButton(
+                              backgroundColor: Colors.grey,
+                              onPressed: () {},
+                              child: Icon(
+                                Icons.add_rounded,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            ),
+                          ]),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 170,
+                width: 150,
+                child: Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Peso',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                          "62",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            FloatingActionButton(
+                              backgroundColor: Colors.grey,
+                              onPressed: () {},
+                              child: Center(
+                                  child: Icon(
+                                Icons.add_rounded,
+                                color: Colors.white,
+                                size: 30,
+                              )),
+                            ),
+                            FloatingActionButton(
+                              backgroundColor: Colors.grey,
+                              onPressed: () {},
+                              child: Icon(
+                                Icons.add_rounded,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            ),
+                          ]),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )),
+        Container(
+          color: Colors.pink,
+          height: 90,
+          child: Row(
+            children: [],
+          ),
+        ),
+        /*  Expanded(
           child: GridView.count(
             primary: false,
             padding: const EdgeInsets.all(20),
@@ -48,7 +246,6 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                color: Colors.teal[100],
               ),
               Container(
                 child: Card(
@@ -68,15 +265,25 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                color: Colors.teal[200],
               ),
               Container(
-                child: const Text('Sound of screams but the'),
-                color: Colors.teal[300],
-              ),
-              Container(
-                child: const Text('Who scream'),
-                color: Colors.teal[400],
+                child: Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image(
+                            image: AssetImage('assets/male.png'),
+                            width: 50,
+                            height: 50),
+                        Text('Hombre', style: TextStyle(color: Colors.white70))
+                      ],
+                    ),
+                  ),
+                ),
               ),
               Container(
                 child: Card(
@@ -124,7 +331,6 @@ class _HomePageState extends State<HomePage> {
                     ],
                   ),
                 ),
-                color: Colors.teal[500],
               ),
               Container(
                 child: Card(
@@ -172,11 +378,17 @@ class _HomePageState extends State<HomePage> {
                     ],
                   ),
                 ),
-                color: Colors.teal[600],
               ),
             ],
           ),
         ),
+        Container(
+          color: Colors.pink,
+          height: 80,
+          child: Row(
+            children: [],
+          ),
+        ),*/
       ],
     );
   }
